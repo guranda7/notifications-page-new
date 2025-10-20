4a2031fd4790da265ed1861812517828f5c83146
guranda <gurandaggg@gmail.com>
2025-10-20 16:38:50 +0400

    create onclick functionality

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,23 @@ function App() {
     setNotifications(updatedNotifications)
  }
 
+ const counter = notifications.filter((notification) => {
+   return !notification.isRed
+}).length;
+
+const markAllAsRead =() => {
+  const updatedNotifications = notifications.map((notification) => {
+    return {...notification, isRed: true};
+  })
+
+  setNotifications(updatedNotifications)
+}
+
   return (
     <>
       <section className="header">
-        <span>Notifications <button>{notifications.length}</button></span>
-        <p>mark all as read</p>
+        <span>Notifications <button>{counter}</button></span>
+        <p onClick={markAllAsRead}>mark all as read</p>
       </section>
       <main className="notifications">
        {notifications.map((notification) => (
